9da700c8dbf06bb2be9f6fdcc57bea9fbb5630e5
bernardo rodrigues <bernardorodrigues@pm.me>
2022-11-18 09:42:19 -0300

    18.11.2022-2

--- view.py
+++ view.py
@@ -5,12 +5,26 @@ import sqlite3 as lite
 con = lite.connect('dados.db')
 
 
-dados = ['vaso', 'sala de estar', 'vaso que comprei no mercado ao lado', 'Marca X', '27/08/2022', '100', 'xxxxxx', 'c:imagens']
+dados = ['sofa', 'sala de estar', 'sofa que comprei em 1997', 'Marca X', '27/08/2022', '500', 'xxxxxx', 'c:imagens']
 # inserir dados
 
+# INSERIR DADOS  
 with con:
     cur = con.cursor()
     query = "INSERT INTO inventario(nome, local, descricao, marca, data_da_compra, valor_da_compra, serie, imagem) VALUES(?,?,?,?,?,?,?,?)"
     cur.execute(query,dados)
 
+ver_dados = []
+
+# VER DADOS    
+with con:
+    cur = con.cursor()
+    query = "SELECT * FROM inventario"
+    cur.execute(query)
+
+    rows = cur.fetchall()
+    for row in rows:
+        ver_dados.append(row)
+
+print(ver_dados)
     
